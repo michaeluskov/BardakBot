f99b74cd524ff8f6fdd6cc9cfedccefcae1aa094
Michael Uskov <uskovm@gmail.com>
2017-04-18 16:31:07 +0500

    Fixed sending message

--- send_message.py
+++ send_message.py
@@ -1,12 +1,20 @@
 ﻿import config
 from telegram import Bot
 import db
+import time
+import pprint
 
 db_users = db.users.getAllUsers()
 user_ids = [x["_id"] for x in db_users]
 
 bot = Bot(config.BOT_TOKEN)
-for i in user_ids:
+for num, i in enumerate(user_ids):
     print("Sending message to %s" % i)
-    bot.sendMessage(i, config.SEND_TEXT)
+    if num % 10 == 0:
+        time.sleep(7)
+    try:
+        bot.sendMessage(i, config.SEND_TEXT)
+    except Exception as e:
+        pprint.pprint(e)
+        
 
